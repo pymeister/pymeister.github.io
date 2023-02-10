c42ebe706098a4eae71b4719f797fee3d7fcc657
Lance Meister <k.lancemeister@gmail.com>
2023-02-10 10:15:32 +0100

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -102,7 +102,7 @@ const config = {
   blog: {
     source: 'dev', // medium | dev
     username: 'pymeister', // to hide blog section, keep it empty
-    limit: 3, // How many posts to display. Max is 10.
+    limit: 2, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
