37b44ab95e9f78ad6515c48499ca878273f8e6c8
Kelechi Meister <k.lancemeister@gmail.com>
2023-02-10 15:36:15 +0100

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -128,7 +128,7 @@ const config = {
   blog: {
     source: 'medium', // medium | dev
     username: 'k.lancemeister', // to hide blog section, keep it empty
-    limit: 10, // How many posts to display. Max is 10.
+    limit: 2, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
