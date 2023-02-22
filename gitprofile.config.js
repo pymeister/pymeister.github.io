75b621384087fa8de9e9f1982ecde1f8cd2eb6c0
Kelechi Meister <k.lancemeister@gmail.com>
2023-02-22 12:22:21 +0100

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -134,7 +134,7 @@ const config = {
     limit: 10, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    id: 'G-M66FTZM6CJ', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
